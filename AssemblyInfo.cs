aeb1cce280a1b36bdfd438a88006cc02353869c9
Jeremy Fabre <jeremy.fabre@ziliko.com>
2012-08-09 15:26:08 -0400

    Adds AssemblyInfo

---
+++ AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyCompany("Ziliko, Inc.")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright (c) 2012 Ziliko, Inc. All Rights Reserved.")]
+[assembly: AssemblyVersion("1.5.1")]
+
